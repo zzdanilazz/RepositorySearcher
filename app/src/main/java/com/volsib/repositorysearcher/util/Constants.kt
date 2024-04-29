f6337df6b9ca7be94308295d98194ee9c5099db6
zzdanilazz <danya.zavalin31102002@gmail.com>
2024-04-29 07:56:31 +0300

    fix: Change the access token in Constants.kt.

--- app/src/main/java/com/volsib/repositorysearcher/util/Constants.kt
+++ app/src/main/java/com/volsib/repositorysearcher/util/Constants.kt
@@ -2,7 +2,7 @@ package com.volsib.repositorysearcher.util
 
 class Constants {
     companion object {
-        const val API_KEY = "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"
+        const val API_KEY = "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"
         const val BASE_URL = "https://api.github.com"
         const val QUERY_PAGE_SIZE = 30
     }
